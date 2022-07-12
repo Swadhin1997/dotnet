775c6f259d4addbf205e12ab2600652cf837e2b2
Swadhin Kumar Nayak <swadhinkumar1997@gmail.com>
2022-07-12 13:03:38 +0530

    hello world

--- dotnet.cs
+++ dotnet.cs
@@ -1,18 +1,8 @@
 using System;
-namespace Add{
-    public class Program{
-        public static int addition(int a, int b){
-            return (a+b);
-        }
-        public static void Main(){
-            int a,b;
-            int sum;
-            Console.Write("Enter first number: ");
-            a = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            b = Convert.ToInt32(Console.ReadLine());
-            sum = addition(a,b);
-            Console.WriteLine("Sum is: " + sum);
-        }
-    }
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello Mono World");
+    }
 }
